78f13d98c140f372044d21610b116ba6d8eb1575
Shahruk Kabir <shahrukkabir01@gmail.com>
2024-01-30 20:25:49 +0600

    first commit

---
+++ solution_for/one.js
@@ -0,0 +1,3 @@
+for (let i = 1; i <= 60; i++) {
+    console.log(`${i}. I will invest at least 6 hrs every single day for the next 60 days!`);
+}
---
+++ solution_while/first.js
@@ -0,0 +1,6 @@
+let i=1;
+while(i<=60){
+    console.log(`${i}. I will invest at least 6 hrs every single day for the next 60 days!`);
+    i++;
+}
+
